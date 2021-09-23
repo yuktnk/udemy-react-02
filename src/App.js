56d7288bc30dcf37d9e99dafb4eb63b28f43b7ca
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-09-23 17:35:52 +0900

    コンポーネントのmomo化と、useCallback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 
 import "./styles.css";
 
 export default function App() {
-  console.log("レンダリング！");
+  console.log("App レンダリング！");
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
 
@@ -15,13 +15,16 @@ export default function App() {
   const onClickOpen = () => {
     setOpen(!open);
   };
+
+  const onClickClose = useCallback(() => setOpen(false), []);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
