65295cdc51d31c4695f5cb862d4773bd8fc44c8e
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-09-26 18:30:48 +0900

    遷移時に遷移元から遷移先にstateを渡す

--- src/Page01.jsx
+++ src/Page01.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 export const Page01 = () => {
+  const array = [...Array(100).keys()];
+  console.log(array);
   return (
     <div>
       <h1>Page01ページ</h1>
-      <Link to="/page01/detailA">DetailA</Link>
+      <Link to={{ pathname: "/page01/detailA", state: array }}>DetailA</Link>
       <br />
       <Link to="/page01/detailB">DetailB</Link>
     </div>
--- src/Page01DetailA.jsx
+++ src/Page01DetailA.jsx
@@ -1,4 +1,7 @@
+import { useLocation } from "react-router-dom";
 export const Page01DetailA = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <h1>Page01DetailAページ</h1>
