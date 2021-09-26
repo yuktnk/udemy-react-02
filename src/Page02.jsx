ba24e5fa2dffe7fabdbec0f9719fa0f5a619fb7a
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-09-26 18:26:09 +0900

    クエリパラメーターを扱う

--- src/Page02.jsx
+++ src/Page02.jsx
@@ -5,6 +5,8 @@ export const Page02 = () => {
     <div>
       <h1>Page02ページ</h1>
       <Link to="page02/999">URL Parameter</Link>
+      <br />
+      <Link to="page02/999?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,10 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
+  console.log(query);
+
   return (
     <div>
       <h1>UrlParameterページ</h1>
       <p>パラメーターは{id}です</p>
+      <p>クエリパラメーターは {query.get("name")} です</p>
     </div>
   );
 };
